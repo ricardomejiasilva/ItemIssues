f16baefc0f0cd1958dd6401b3459664383d58fcf
John Ness <jness@webstaurantstore.com>
2022-04-12 08:50:55 -0400

    Add initial item issues page

---
+++ src/ItemIssues.Web/Features/ItemIssues/Assets/ItemIssuesViewAsset.cs
@@ -0,0 +1,13 @@
+﻿using ItemIssues.Web.Features.MicroFrontend.Definitions;
+
+namespace ItemIssues.Web.Features.ItemIssues.Assets
+{
+    public class ItemIssuesViewAsset : ViewAssetDefinitionBase
+    {
+        public override string ViewName => "ItemIssues";
+
+        public override IEnumerable<string> Scripts => new[] { "itemIssues/itemIssues.js" };
+
+        public override IEnumerable<string> Styles => new[] { "itemIssues/itemIssues.css" };
+    }
+}
